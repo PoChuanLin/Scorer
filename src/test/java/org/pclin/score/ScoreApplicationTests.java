28ec50747422f751b1223da4c81416f7b06caab7
Po-Chuan Lin <pclin.java@gmail.com>
2020-02-24 09:08:57 -0600

    add 1 test, data files.

---
+++ src/test/java/org/pclin/score/ScoreApplicationTests.java
@@ -0,0 +1,12 @@
+package org.pclin.score;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(args = "data_blank.csv")
+class ScoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
