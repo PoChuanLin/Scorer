14d184f2624fa6b5f0f20e5267e4437b3e536d91
Po-Chuan Lin <pclin.java@gmail.com>
2020-10-04 18:09:28 -0500

    add Gradle properties file.

--- src/test/java/org/pclin/score/ScoreApplicationTests.java
+++ src/test/java/org/pclin/score/ScoreApplicationTests.java
@@ -6,7 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest(args = "data_blank.csv")
 class ScoreApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 }
